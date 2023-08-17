b1421b92bfdba6536571df9ab9fd566643e282ee
Sebastian Sauerer <s.sauerer@epilot.cloud>
2023-08-17 16:34:05 +0200

    feat: change example of handler usage in api route

--- examples/playground/pages/api/image-queue.ts
+++ examples/playground/pages/api/image-queue.ts
@@ -1,15 +1,17 @@
 import { Queue } from "@serverlessq/nextjs";
 import { NextApiRequest, NextApiResponse } from "next";
 
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log("Message arrived!", req.body)
+  return res.status(200).json({ status: "Image transformed ✅ - 500 KB " });
+}
+
 export default Queue({
   options: {
     name: "image-queue",
     route: "api/image-queue",
     retries: 3,
   },
-  handler: async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log("Message arrived!", req.body)
-    return res.status(200).json({ status: "Image transformed ✅ - 500 KB " });
-  },
+  handler
 });
 
--- examples/playground/pages/api/newsletter-queue.ts
+++ examples/playground/pages/api/newsletter-queue.ts
@@ -1,15 +1,17 @@
 import { Queue } from "@serverlessq/nextjs";
 import { NextApiRequest, NextApiResponse } from "next";
 
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log("Message arrived!", req.body)
+  return res.status(200).json({ status: "Image transformed ✅ - 500 KB " });
+}
+
 export default Queue({
   options: {
     name: "newsletter-queue",
     route: "api/newsletter-queue",
     retries: 3,
   },
-  handler: async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log("Message arrived!", req.body);
-    return res.status(200).json({ status: "Image transformed ✅" });
-  },
+  handler
 });
 
