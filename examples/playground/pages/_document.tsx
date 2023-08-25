399632c38d649fca56bc3731bc1ae13df41ac844
Florian Fuchs <florian@hashnode.com>
2023-08-25 13:12:10 +0200

    refactor: removed unnecessary stuff

--- examples/playground/pages/_document.tsx
+++ examples/playground/pages/_document.tsx
@@ -1,16 +1,4 @@
-import { serverlessQExit } from '@serverlessq/nextjs';
 import { Html, Head, Main, NextScript } from 'next/document'
-
-// this should be added in your custom _document
-process.on("SIGTERM", () => {
-  console.log("Received SIGTERM: ", "cleaning up");
-  serverlessQExit(1);
-});
-process.on("SIGINT", () => {
-  console.log("Received SIGINT: ", "cleaning up");
-  serverlessQExit(1);
-});
-
 
 export default function Document() {
   return (
